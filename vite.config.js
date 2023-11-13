d0a77fc63fcfcd77e8a543c11304969d8b227d8e
DESKTOP-PJGE4NP\Studio-Desktop <joe.hunter.dev@gmail.com>
2023-11-14 00:42:12 +0100

    updates for build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://joehunter.dev/react-pdf-cv/',
+  base: '',
   plugins: [react()],
 })
